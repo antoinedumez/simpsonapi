cb982e553797f7c68b90e9f69a90a1093ef78f54
Antoine Dumez <MY_NAME@example.com>
2022-05-01 15:55:40 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8" />
+    <meta
+        name="viewport"
+        content="width=device-width, initial-scale=1, shrink-to-fit=no"
+    />
+
+    <title>Ajax quest challenge</title>
+    <link rel="icon" type="image/png" href="favicon.png" />
+    <link
+        rel="stylesheet"
+        href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css"
+    />
+    <link
+        rel="stylesheet"
+        href="https://unpkg.com/bulma@0.7.5/css/bulma.min.css"
+    />
+    <link
+        rel="stylesheet"
+        href="https://use.fontawesome.com/releases/v5.5.0/css/all.css"
+        integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU"
+        crossorigin="anonymous"
+    />
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+    <style></style>
+</head>
+
+<body>
+    <section class="section">
+        <div class="container">
+            <h1 class="title">Simpson API</h1>
+
+            <div class="content" id="simpson_quote"></div>
+            <button type="button" class="btn btn-primary btn-lg" id="myBtn">New citation</button>
+        </div>
+    </section>
+    <p id="demo">
+
+
+
+
+    <!-- We need to load axios first! -->
+    <script
+        src="https://cdnjs.cloudflare.com/ajax/libs/axios/0.19.0/axios.min.js"
+        integrity="sha256-S1J4GVHHDMiirir9qsXWc8ZWw74PHHafpsHp5PXtjTs="
+        crossorigin="anonymous"
+    ></script>
+    <script>
+        // This function loads quotes data from the simpson quote API
+        function fetchSimpsonQuoteJSON() {
+            const url = `https://simpsons-quotes-api.herokuapp.com/quotes/`;
+            axios.get(url)
+                .then(function(response) {
+                    return response.data; // response.data instead of response.json() with fetch
+                })
+                .then(function(simpsonQuote) {
+                    console.log('data decoded from JSON:', simpsonQuote);
+
+                    // Build a block of HTML
+                    const simpsonQuoteHtml = `
+        <p><strong>${simpsonQuote[0].character}</strong></p>
+        <p><strong>${simpsonQuote[0].quote}</strong></p>
+        <img src="${simpsonQuote[0].image}" />
+
+      `;
+                    document.querySelector('#simpson_quote').innerHTML = simpsonQuoteHtml;
+                });
+        }
+
+        const button = document.getElementById("myBtn");
+        button.addEventListener("click", function() {
+            fetchSimpsonQuoteJSON();
+        });
+
+
+
+        fetchSimpsonQuoteJSON();
+
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+</body>
+</html>
